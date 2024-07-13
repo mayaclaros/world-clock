9a00c3e0d9f5f32ffe4eb5247fa470c03b2d0d19
mayaclaros <123900092+mayaclaros@users.noreply.github.com>
2024-07-12 23:53:58 -0400

    Re-worked js and html

--- src/index.js
+++ src/index.js
@@ -1,38 +1,39 @@
-function updateTime() {
-  let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
-  losAngelesDateElement.innerHTML = moment().format("MMMM D, YYYY");
-  losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
-    "h:mm:ss"
-  )} <small>${losAngelesTime.format("A")}</small> `;
+//Los Angeles
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  let sanSalvadorElement = document.querySelector("#san-salvador");
-  let sanSalvadorDateElement = sanSalvadorElement.querySelector(".date");
-  let sanSalvadorTimeElement = sanSalvadorElement.querySelector(".time");
-  let sanSalvadorTime = moment().tz("America/El_Salvador");
-  sanSalvadorDateElement.innerHTML = moment().format("MMMM D, YYYY");
-  sanSalvadorTimeElement.innerHTML = `${sanSalvadorTime.format(
-    "h:mm:ss"
-  )} <small>${sanSalvadorTime.format("A")}</small> `;
+losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM D, YYYY");
+losAngelesTimeElement.innerHTML = losAngelesTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
 
-  let lagosElement = document.querySelector("#lagos");
-  let lagosDateElement = lagosElement.querySelector(".date");
-  let lagosTimeElement = lagosElement.querySelector(".time");
-  let lagosTime = moment().tz("Africa/Lagos");
-  lagosDateElement.innerHTML = moment().format("MMMM D, YYYY");
-  lagosTimeElement.innerHTML = `${lagosTime.format(
-    "h:mm:ss"
-  )} <small>${lagosTime.format("A")}</small> `;
+//San Salvador
+let sanSalvadorElement = document.querySelector("#san-salvador");
+let sanSalvadorDateElement = sanSalvadorElement.querySelector(".date");
+let sanSalvadorTimeElement = sanSalvadorElement.querySelector(".time");
+let sanSalvadorTime = moment().tz("America/El_Salvador");
 
-  let manilaElement = document.querySelector("#manila");
-  let manilaDateElement = manilaElement.querySelector(".date");
-  let manilaTimeElement = manilaElement.querySelector(".time");
-  let manilaTime = moment().tz("Asia/Manila");
-  manilaDateElement.innerHTML = moment().format("MMMM D, YYYY");
-  manilaTimeElement.innerHTML = `${manilaTime.format(
-    "h:mm:ss"
-  )} <small>${manilaTime.format("A")}</small>`;
-}
-setInterval(updateTime, 1000);
+sanSalvadorDateElement.innerHTML = sanSalvadorTime.format("MMMM D, YYYY");
+sanSalvadorTimeElement.innerHTML = sanSalvadorTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
+
+// Lagos
+let lagosElement = document.querySelector("#lagos");
+let lagosDateElement = lagosElement.querySelector(".date");
+let lagosTimeElement = lagosElement.querySelector(".time");
+let lagosTime = moment().tz("Africa/Lagos");
+
+lagosDateElement.innerHTML = lagosTime.format("MMMM D, YYYY");
+lagosTimeElement.innerHTML = lagosTime.format("h:mm:ss [<small>]A[</small>]");
+
+//Manila
+let manilaElement = document.querySelector("#manila");
+let manilaDateElement = manilaElement.querySelector(".date");
+let manilaTimeElement = manilaElement.querySelector(".time");
+let manilaTime = moment().tz("Asia/Manila");
+
+manilaDateElement.innerHTML = manilaTime.format("MMMM D, YYYY");
+manilaTimeElement.innerHTML = manilaTime.format("h:mm:ss [<small>]A[</small>]");
